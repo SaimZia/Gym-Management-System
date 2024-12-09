2c2c657cb14112b0fb4036cce46a4398ac33ca52
yahyachughtai <107436277+yahyachughtai@users.noreply.github.com>
2024-12-09 05:58:25 +0500

    test push

--- backend/controllers/management/customerController.js
+++ backend/controllers/management/customerController.js
@@ -1,106 +1,105 @@
 // controllers/management/customerController.js
-const User = require('../../models/User');
-const Subscription = require('../../models/Subscription');
+const User = require("../../models/User");
+const Subscription = require("../../models/Subscription");
 
 // List all customers
 exports.listCustomers = async (req, res) => {
-    try {
-        const customers = await User.find({ role: 'customer' })
-            .select('-password');
-        res.json({
-            success: true,
-            data: customers
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
-    }
+  try {
+    const customers = await User.find({ role: "customer" }).select("-password");
+    res.json({
+      success: true,
+      data: customers,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 // Get single customer details
 exports.getCustomerDetails = async (req, res) => {
-    try {
-        const customer = await User.findOne({
-            _id: req.params.id,
-            role: 'customer'
-        }).select('-password');
+  try {
+    const customer = await User.findOne({
+      _id: req.params.id,
+      role: "customer",
+    }).select("-password");
 
-        if (!customer) {
-            return res.status(404).json({
-                success: false,
-                message: 'Customer not found'
-            });
-        }
-
-        res.json({
-            success: true,
-            data: customer
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
+    if (!customer) {
+      return res.status(404).json({
+        success: false,
+        message: "Customer not found",
+      });
     }
+
+    res.json({
+      success: true,
+      data: customer,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 // Update customer
 exports.updateCustomer = async (req, res) => {
-    try {
-        const customer = await User.findOneAndUpdate(
-            { _id: req.params.id, role: 'customer' },
-            req.body,
-            { new: true }
-        ).select('-password');
+  try {
+    const customer = await User.findOneAndUpdate(
+      { _id: req.params.id, role: "customer" },
+      req.body,
+      { new: true }
+    ).select("-password");
 
-        if (!customer) {
-            return res.status(404).json({
-                success: false,
-                message: 'Customer not found'
-            });
-        }
-
-        res.json({
-            success: true,
-            data: customer
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
+    if (!customer) {
+      return res.status(404).json({
+        success: false,
+        message: "Customer not found",
+      });
     }
+
+    res.json({
+      success: true,
+      data: customer,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 // Get customer's attendance
 exports.getCustomerAttendance = async (req, res) => {
-    try {
-        const customer = await User.findOne({
-            _id: req.params.id,
-            role: 'customer'
-        });
+  try {
+    const customer = await User.findOne({
+      _id: req.params.id,
+      role: "customer",
+    });
 
-        if (!customer) {
-            return res.status(404).json({
-                success: false,
-                message: 'Customer not found'
-            });
-        }
-
-        // Placeholder for attendance data
-        res.json({
-            success: true,
-            data: {
-                customerId: req.params.id,
-                attendance: []
-            }
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
+    if (!customer) {
+      return res.status(404).json({
+        success: false,
+        message: "Customer not found",
+      });
     }
+
+    // Placeholder for attendance data
+    res.json({
+      success: true,
+      data: {
+        customerId: req.params.id,
+        attendance: [],
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
